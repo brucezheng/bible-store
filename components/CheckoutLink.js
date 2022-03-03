ee000a18edfe7d5c15c23de17fe800f90fe14f63
Bruce Zheng <bruce.c.zheng@gmail.com>
2022-03-02 19:32:46 -0600

    Disable paying with empty cart

--- components/CheckoutLink.js
+++ components/CheckoutLink.js
@@ -1,12 +1,20 @@
 import Link from 'next/link'
-import { getEncodedCartItems } from '../providers/CartProvider'
+import { getEncodedCartItems, getCartItems } from '../providers/CartProvider'
+import { calculateTotal } from '../utils/Utils'
 import styles from '../styles/Home.module.css'
 
 export default function CheckoutLink() {
 	const href = '/checkout?cart=' + getEncodedCartItems();
+	if (calculateTotal(getCartItems()) > 0.5) {
+		return (
+	            <Link href={href}>
+	              <a className={styles.card}>Pay</a>
+	            </Link>
+	    );
+	}
 	return (
-            <Link href={href}>
-              <a className={styles.card}>Pay</a>
+            <Link href='/'>
+              <a className={styles.card}>Return to Store</a>
             </Link>
     );
 }
